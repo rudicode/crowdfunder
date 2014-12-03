3cab433c8a7401f56b76fb98b2872276c592020f
Rudi Luzar <rudi@rudicode.com>
2014-12-03 15:32:20 -0500

    Added projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,16 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all  
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# salt = "asdasdastr4325234324sdfds"
+# crypted_password = Sorcery::CryptoProviders::BCrypt.encrypt("a", salt)
+User.destroy_all
+
+10.times do |num|
+
+  User.create!({
+    first_name: "Homer#{num}",
+    last_name:  "Simpson",
+    email:      "homer#{num}@powerplant.org",
+    password:   "a"
+    })
+end
+
+
+
+Project.create!({
+   owner_id: 1,
+   title: "Carbon Rod",
+   description: "Creating personalized carbon rods for you all fans!!"
+  })
