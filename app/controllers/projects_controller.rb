eb6f84643d5343841b34334c4ef10a6c0b73f917
Rudi Luzar <rudi@rudicode.com>
2014-12-03 16:16:32 -0500

    Creating new projects with Projects#new project#create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,9 +9,20 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
   end
 
   def update
   end
 
+private
+
+def project_params
+  params.require(:project).permit(:title, :description, :goal)
+end
 end
