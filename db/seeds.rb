0aa58da7bd27c4e0958134505f80d41815333bfa
Rudi Luzar <rudi@rudicode.com>
2014-12-05 14:22:17 -0500

    Fixed typo in seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,6 @@ end
 Project.create!({
    owner_id: 1,
    title: "ProtoFlux - The First Magnetic Filament For Your 3D Printer",
-   description: "Attract magnets and store data with ProtoFlux Magnetic Filament, a new ProtoTek 3D Printing Material."
+   description: "Attract magnets and store data with ProtoFlux Magnetic Filament, a new ProtoTek 3D Printing Material.",
    picture: "http://upload.wikimedia.org/wikipedia/commons/3/3d/Little_Gasparilla_sunrise.jpg"
   })
