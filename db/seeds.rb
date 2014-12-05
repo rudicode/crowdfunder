de6619f3ba0d645a642186734129ee137e9130fa
Rudi Luzar <rudi@rudicode.com>
2014-12-05 15:16:35 -0500

    seeds file generates projects from an array of image urls

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,17 +22,49 @@ Project.destroy_all
 #     })
 # end
 
-user = User.create!({
-    first_name: "ProtoParadigm",
-    last_name:  ".",
-    email:      "ProtoParadigm@powerplant.org",
-    password:   "a",
-    password_confirmation: "a"
+urls = [
+"https://s3.amazonaws.com/ksr/projects/1307182/photo-carousel.jpg",
+"https://s3.amazonaws.com/ksr/projects/1430513/photo-carousel.jpg",
+"https://s3.amazonaws.com/ksr/projects/1252028/photo-carousel.jpg",
+"https://s3.amazonaws.com/ksr/projects/914389/photo-carousel.jpg",
+"https://s3.amazonaws.com/ksr/projects/1420032/photo-carousel.jpg",
+"https://s3.amazonaws.com/ksr/projects/1475293/photo-ed.jpg",
+"https://s3.amazonaws.com/ksr/projects/1014954/photo-ed.jpg",
+"https://s3.amazonaws.com/ksr/projects/1376382/photo-ed.jpg",
+"https://s3.amazonaws.com/ksr/projects/1515743/photo-ed.jpg",
+"https://s3.amazonaws.com/ksr/projects/1452552/photo-ed.jpg",
+"https://s3.amazonaws.com/ksr/projects/1489223/photo-ed.jpg",
+"https://s3.amazonaws.com/ksr/projects/1522779/photo-ed.jpg",
+"https://s3.amazonaws.com/ksr/projects/1340592/photo-ed.jpg",
+"https://s3.amazonaws.com/ksr/projects/1397635/photo-ed.jpg"
+
+]
+
+urls.each_with_index do |url,count|
+
+  user = User.create!({
+      first_name: "ProtoParadigm#{count}",
+      last_name:  ".",
+      email:      "ProtoParadigm#{count}@powerplant.org",
+      password:   "a",
+      password_confirmation: "a"
+      })
+
+  project = Project.create!({
+     owner_id: user.id,
+     goal: 6000,
+     start_date: Time.now,
+     finish_date:   Time.now + 30.days,
+     title: "ProtoFlux#{count} - The First Magnetic Filament For Your 3D Printer",
+     description: "#{count} Attract magnets and store data with ProtoFlux Magnetic Filament, a new ProtoTek 3D Printing Material.",
+     picture: url
     })
 
-Project.create!({
-   owner_id: user.id,
-   title: "ProtoFlux - The First Magnetic Filament For Your 3D Printer",
-   description: "Attract magnets and store data with ProtoFlux Magnetic Filament, a new ProtoTek 3D Printing Material.",
-   picture: "https://s3.amazonaws.com/ksr/projects/1307182/photo-carousel.jpg"
-  })
+  6.times do |index|
+    Reward.create!({
+      min_amount: 500 * index+1,
+      description: "Reward #{index+1}",
+      project_id: project.id
+      })
+  end
+end
