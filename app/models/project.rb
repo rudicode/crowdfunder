b2281201b62d2c99c193339bc215352197e970d5
Joshua Sebastian <jbs87@hotmail.com>
2014-12-05 15:28:57 -0500

    User profile backed projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
-  belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
+  belongs_to :owner, class_name: 'User'# , foreign_key: 'owner_id'
   has_many :pledges
-  has_many :backers, through: :pledges, class_name: 'User'
+  has_many :backers, through: :pledges, source: "user"
   has_many :rewards
   
   accepts_nested_attributes_for :rewards, :reject_if => :all_blank
